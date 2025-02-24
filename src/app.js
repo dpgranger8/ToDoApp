8196bf643ec0b636f695b2553486e28a200385d7
David Granger <dpgranger8@gmail.com>
2025-02-24 10:42:43 -0700

    changed to use grid instead of rows

--- src/app.js
+++ src/app.js
@@ -1,30 +1,37 @@
-const hello = "hello world";
-
-let list = ["I", "Am", "a", "list"];
+const container = document.getElementById("listContainer");
+let list = [
+  "Buy groceries",
+  "Call mom",
+  "Workout",
+  "Read a book",
+  "Plan weekend trip",
+  "Reply to emails",
+  "Water plants",
+  "Learn JavaScript",
+  "Cook dinner"
+];
 
 (() => {
     populateList();
 })();
 
 function createToDo() {
-    let toDoText = document.getElementById("toDoInput");
-    list.push(toDoText.textContent);
+    let toDoInput = document.getElementById("toDoInput");
+    list.push(toDoInput.value);
+    toDoInput.value = "";
+    container.innerHTML = "";
+    populateList();
 }
 
 function createList() {
     let listTitle = document.getElementById("listInput");
 }
 
-function listChanged() {
-    
-}
-
 function populateList() {
-    const container = document.getElementById("listContainer");
     let borderDiv = document.createElement("div");
     borderDiv.classList.add("flex", "justify-center", "items-center", "p-1", "h-12", "bg-linear-to-r", "from-cyan-500/50", "to-blue-500/50", "rounded-2xl", "backdrop-blur-xs");
     let contentDiv = document.createElement("div");
-    contentDiv.classList.add("flex", "justify-center", "items-center", "bg-white/20", "w-full", "h-full", "rounded-xl", "backdrop-blur-xs");
+    contentDiv.classList.add("flex", "items-center", "bg-white/20", "w-full", "h-full", "rounded-xl", "backdrop-blur-xs", "pl-5");
     list.forEach(element => {
         let borderClone = borderDiv.cloneNode();
         let contentClone = contentDiv.cloneNode();
